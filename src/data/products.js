fc6ea20f7f6cd799c1067813b605d1c3bcc5de87
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-10-31 18:07:05 +0530

    Images sent into the assests/website_image directory"

--- src/data/products.js
+++ src/data/products.js
@@ -1,21 +1,21 @@
 export const products = [
 {
 id: 1,
-image:,
+image:'',
 name: 'Core i5 Laptop - 8GB RAM / 512GB SSD',
 price: '₹45,000',
 short: 'Reliable everyday performance',
 },
 {
 id: 2,
-image:,
+image:'',
 name: 'Gaming Desktop - Ryzen 7 / RTX 4060',
 price: '₹95,000',
 short: 'High FPS gaming & streaming',
 },
 {
 id: 3,
-image:,
+image:'',
 name: 'Wireless Mouse',
 price: '₹799',
 short: 'Ergonomic, long battery life',
